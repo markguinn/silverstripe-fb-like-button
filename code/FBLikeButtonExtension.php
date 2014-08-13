94742502d04a42cf83d43e397d5d76d33b4b12d6
Jeremy Shipman <jeremy@burnbright.net>
2014-08-13 13:26:14 +1200

    NEW: Allow specifying no application id

--- code/FBLikeButtonExtension.php
+++ code/FBLikeButtonExtension.php
@@ -55,13 +55,15 @@ class FBLikeButtonExtension extends SiteTreeExtension
 	 */
 	public function FacebookJSSDK() {
 		$appID = $this->getAppID();
+		$appID = empty($appID) ? "" : "&appId=".$appID;
+
 		return <<<HTML
 				<div id="fb-root"></div>
 				<script>(function(d, s, id) {
 				  var js, fjs = d.getElementsByTagName(s)[0];
 				  if (d.getElementById(id)) return;
 				  js = d.createElement(s); js.id = id;
-				  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId={$appID}";
+				  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1{$appID}";
 				  fjs.parentNode.insertBefore(js, fjs);
 				}(document, 'script', 'facebook-jssdk'));</script>
 HTML
